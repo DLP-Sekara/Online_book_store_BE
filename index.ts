1408b20b2159441be2d2c2342077d996a6d31847
Lahiru Piyumal <dslahirupiyumal99@gmail.com>
2024-12-22 12:31:24 +0530

    intial configs in index.ts file

--- index.ts
+++ index.ts
@@ -1,49 +1,42 @@
-// import express from 'express';
-// import mongoose from 'mongoose';
-// const app = express();
-// import cors from 'cors';
-// import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
-// // import bookRoute from './src/routes/bookRoute';
-// // import orderRoute from './src/routes/orderRoute';
-// // import customerRoute from './src/routes/customerRoute';
-// // import cookieParser from 'cookie-parser';
+dotenv.config();
 
-// dotenv.config();
-// const port = process.env.PORT;
+const app = express();
+const port = process.env.PORT;
 
-// app.use(express.json());
-// app.use(cookieParser());
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "*",
+    methods: "*",
+    credentials: true,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 
-// app.use(
-//    cors({
-//       origin: "*",
-//       methods: "*",
-//       credentials: true,
-//    })
-// );
-  
-// app.use(express.urlencoded({ extended: true }));
+// MongoDB connection
+const url: any = process.env.DATABASE_URL;
+mongoose.connect(url);
+const con = mongoose.connection;
+con.on("error", console.error.bind(console, "MongoDB connection error:"));
+con.once("open", () => {
+  console.log("MongoDB connected successfully");
+});
 
-// //routes
-// app.use('/book',bookRoute);
-// app.use('/customer',customerRoute);
-// app.use('/order',orderRoute);
+// Routes
+app.get("/", (req, res) => {
+  console.log("GET request received");
+  res.send("Hello, MongoDB!");
+});
 
-// //mongoDb connection 
-// const url=process.env.DATABASE_URL;
-// mongoose.connect(url);
-// const con=mongoose.connection;
-// con.on('error', console.error.bind(console, 'MongoDB connection error:'));
-// con.once('open', () => {
-//   console.log('MongoDB connected successfully');
-// });
-
-// app.get('/',(req,res)=>{
-//   console.log('get request has come');
-//   res.send('hello mongo');
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
